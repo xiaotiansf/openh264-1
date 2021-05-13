7a661aa619d9a68f6d1bfee2d1630d7ff6c0327a
xiaotianshimail <xiaotianshimail@gmail.com>
2021-05-13 16:16:48 -0700

    fix crash.

--- codec/decoder/core/src/decoder.cpp
+++ codec/decoder/core/src/decoder.cpp
@@ -305,8 +305,8 @@ void ResetReorderingPictureBuffers (PPictReoderingStatus pPictReoderingStatus, P
       pPictInfo[i].iPOC = IMinInt32;
     }
     pPictInfo->sBufferInfo.iBufferStatus = 0;
+		pPictReoderingStatus->bHasBSlice = false;
   }
-  pPictReoderingStatus->bHasBSlice = false;
 }
 
 /*
